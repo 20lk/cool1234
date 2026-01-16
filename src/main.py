6ac4fef992739c9fa8305897785f5394cef92086
20lk <20lkirumira@fws.kevibham.org>
2026-01-16 09:55:57 +0000

    feat: add example images

--- src/main.py
+++ src/main.py
@@ -1,6 +1,2 @@
-import os
-from time import sleep
-from picamera import PiCamera
-camera = PiCamera()
-camera.resolution = (2028, 1520)  # Half the maximum resolution
-camera.start_preview()
+from exif import Image
+from datetime import datetime
