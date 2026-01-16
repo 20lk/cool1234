4f2f51e1e5948f7a769a027f30fc9f325c5913ea
20lk <20lkirumira@fws.kevibham.org>
2026-01-16 10:29:17 +0000

    feat: add relpaths for images

--- src/iss_speed.py
+++ src/iss_speed.py
@@ -2,6 +2,7 @@ from exif import Image
 from datetime import datetime
 import cv2
 import math
+from pathlib import path
 
 def get_time(image):
     with open(image, 'rb') as image_file:
@@ -35,8 +36,10 @@ def calculate_matches(descriptors_1, descriptors_2):
     matches = sorted(matches, key=lambda x: x.distance)
     return matches
 
-image_1 = 'atlas_photo_012.jpg'
-image_2 = 'atlas_photo_013.jpg'
+here = Path(__file__).resolve().parent
+image_path = here.parent / "assets" 
+image_1 = image_path / 'atlas_photo_012.jpg'
+image_2 = image_path /'atlas_photo_013.jpg'
 
 
 time_difference = get_time_difference(image_1, image_2) # Get time difference between images
